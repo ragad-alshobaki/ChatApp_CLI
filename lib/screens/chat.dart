b2fcbb1aaefc33227168fb832c88d52de09b972e
ragad-alshobaki <rgd.alshobaki@gmail.com>
2025-03-08 02:23:17 +0300

    SignOut

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatelessWidget {
@@ -6,7 +7,18 @@ class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Flutter Chat'),),
+      appBar: AppBar(
+        title: Text('Flutter Chat'),
+        actions: [
+          //Signout:
+          IconButton(onPressed: (){
+            FirebaseAuth.instance.signOut();
+          },
+          icon: Icon(Icons.exit_to_app),
+          color: Theme.of(context).colorScheme.primary,
+        )
+        ],
+      ),
       body: Center(
         child: Text('Logged in!'),
       ),
